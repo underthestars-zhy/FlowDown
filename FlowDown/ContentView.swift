3b14ff666c0f4bad9fad3df32c745a3221d656e0
Underthestars-zhy <zhuhaoyu0909@icloud.com>
2024-02-04 21:43:01 +0800

    Initial Commit

---
+++ FlowDown/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FlowDown
+//
+//  Created by 朱浩宇 on 2024/2/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FlowDown/FlowDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowDownApp.swift
+//  FlowDown
+//
+//  Created by 朱浩宇 on 2024/2/4.
+//
+
+import SwiftUI
+
+@main
+struct FlowDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
